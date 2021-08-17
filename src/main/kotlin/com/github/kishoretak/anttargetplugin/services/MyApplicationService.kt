0d0ddff9871dd608be4b0f6b39a13661f27ed8f4
GitHub Action <action@github.com>
2021-08-17 15:39:16 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/kishoretak/anttargetplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.kishoretak.anttargetplugin.services
+
+import com.github.kishoretak.anttargetplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
